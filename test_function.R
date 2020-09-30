aefda121eee6ef8950358507d631b965c138588e
levlewisSCG <72113377+levlewisSCG@users.noreply.github.com>
2020-09-29 19:22:33 -0700

    Add files via upload

---
+++ test_function.R
@@ -0,0 +1,9 @@
+test <- function(a,b) {
+  return(a+b)
+}
+
+
+#library(downloader) # Library to use url download as source code
+#url <- "https://raw.githubusercontent.com/levlewisSCG/R_Source_Private/master/test_function.R?token=ARGFZYIP2JVLKWSB3DTMZRK7OPMOO"
+#source_url(url, prompt=FALSE) #Set file as source code for this script
+#test(1,4)
