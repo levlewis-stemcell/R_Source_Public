2e754068fae5e1223b32f1e9de85f9e44a8fdcbb
levlewisSCG <72113377+levlewisSCG@users.noreply.github.com>
2020-09-29 19:31:19 -0700

    Add files via upload
    Changed comment code to include url for public repo instead of private repo

--- test_function.R
+++ test_function.R
@@ -4,6 +4,6 @@ test <- function(a,b) {
 
 
 #library(downloader) # Library to use url download as source code
-#url <- "https://raw.githubusercontent.com/levlewisSCG/R_Source_Private/master/test_function.R?token=ARGFZYIP2JVLKWSB3DTMZRK7OPMOO"
+#url <- "https://raw.githubusercontent.com/levlewisSCG/R_Source_Public/master/test_function.R"
 #source_url(url, prompt=FALSE) #Set file as source code for this script
 #test(1,4)
